c7736900bd64fdeb0385b32ac504f9f34f93f3b8
Natasha <a1@MacBook-Pro-Natasha.local>
2022-09-06 17:43:09 +0300

    Modified

--- Task_47/Program.cs
+++ Task_47/Program.cs
@@ -4,10 +4,13 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 Console.WriteLine($"Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами");
+
 Console.Write("Введите m: ");
 int m = Convert.ToInt32(Console.ReadLine());
+
 Console.Write("Введите n: ");
 int n = Convert.ToInt32(Console.ReadLine());
+
 Console.WriteLine($"m = {m}, n = {n}.");
 double[,] array = new double[m, n];
 
--- Task_50/Program.cs
+++ Task_50/Program.cs
@@ -6,29 +6,46 @@
 // 8 4 2 4
 // 1, 7 -> такого числа в массиве нет
 
-Console.Write("Введите m: ");
+Console.Write("Введите m - число строк массива: ");
 int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите n: ");
+
+Console.Write("Введите n - число столбцов массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"m = {m}, n = {n}.");
+
+Console.WriteLine("Введите координаты");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
 int[,] array = new int[n, m];
-Random random = new Random();
-for (int i = 0; i < n; i++)
+
+newArray(array);
+FindByIndex(array, a, b);
+
+void newArray(int[,] array)
 {
+    Random random = new Random();
+    for (int i = 0; i < n; i++)
+    {
     for (int j = 0; j < m; j++)
     {
         array[i, j] = random.Next(10);
         Console.Write("{0}" + " ", array[i, j]);
     }
     Console.WriteLine();
+    }
 }
-Console.WriteLine("Введите координаты");
-int a = Convert.ToInt32(Console.ReadLine());
-int b = Convert.ToInt32(Console.ReadLine());
-if (a>m && b>n)
-Console.WriteLine("такого числа нет");
-else
+
+int FindByIndex(int [,] array, int a, int b)
 {
-object c = array.GetValue(a,b);
-Console.WriteLine(c);
+    if (array.GetLength(0)>a && array.GetLength(1)>b) 
+    {
+        int result = array [a,b];
+        Console.WriteLine($"Значение элемента с индексом ({a},{b}): {result}");
+        return result;
+    }
+    else 
+    {
+        Console.WriteLine("Такого числа в массиве нет");
+        return -1;
+    }
 }
